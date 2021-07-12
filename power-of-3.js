d707256b0716626ed7b2afc9c4ba386b9754d466
MGubernick <MGubernick61@gmail.com>
2021-07-12 12:50:59 -0500

    Add Power-of-3

---
+++ power-of-3.js
@@ -0,0 +1,15 @@
+var isPowerOfThree = function(n) {
+  if (n < 1) {
+    return false
+  }
+  
+  while (n % 3 === 0) {
+    n /= 3
+  }
+  
+  if (n === 1) {
+    return true
+  } else {
+    return false
+  }
+};
