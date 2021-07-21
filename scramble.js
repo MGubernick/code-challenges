2f5c097999c6fdd7f312e419fd9e3dd94044fc74
MGubernick <MGubernick61@gmail.com>
2021-07-21 16:08:55 -0500

    Add Scramblier

--- scramble.js
+++ scramble.js
@@ -3,21 +3,20 @@
 // return true or false
 
 function scramble(str1, str2) {
-  //   console.log('str1: ', str1)
-    console.log('str2: ', str2)
-    const splitWord = str2.split('')
-    const splitLetters = str1.split('').sort()
-    let same = []
-    
-    splitWord.forEach(letter => {
-      if (splitLetters.includes(letter)) {
-        same.push(letter)
-        console.log('same: ', same.join(''))
-      }
-    })
-    if (same.join('') === str2) {
-      return true
+  const splitWord = str2.split('')
+  const splitLetters = str1.split('').sort()
+  let same = []
+  
+  splitWord.forEach(letter => {
+    if (splitLetters.includes(letter)) {
+      let idx = splitLetters.indexOf(letter)
+      same.push(splitLetters.splice(idx, 1))
     }
-    return false
-    
+  })
+  
+  if (same.join('') === str2) {
+    return true
   }
+  return false
+  
+}
